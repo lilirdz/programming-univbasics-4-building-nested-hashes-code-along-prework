bce437babcf5d2b864677c12d3c4a20f3c9d7da8
Lili Rodriguez <lilimrod@gmail.com>
2020-03-12 04:51:24 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,10 +14,11 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :hero_friends => [{
-        :name => "Benvolio",
-        :age => "17",
-        :attitude => "worried"
+      :hero_friends => [
+        {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
         },
         {
           :name => "Mercutio",
